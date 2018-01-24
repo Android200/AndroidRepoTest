c8bddb33e0b4d68f42175bfb2e71265e3374d32d
INTRUD£R\Umar Saidu Auna <umarmanofpeace@gmail.com>
2018-01-24 12:53:02 +0100

    I added a button and makes it clickable by calling set OnClickListener Method

--- app/src/main/java/com/gdgminna/myapplication/MainActivity.java
+++ app/src/main/java/com/gdgminna/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.gdgminna.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Welcome User", Toast.LENGTH_SHORT).show();
+        Button showToast = findViewById(R.id.clickme);
+        showToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome User", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
     }
